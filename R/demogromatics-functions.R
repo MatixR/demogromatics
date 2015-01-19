24bdc07e7d2fa5a08dfc0a735144146bdc77134d
nancyorgan <nancyorgan@gmail.com>
2015-01-19 11:39:50 -0500

    added helper functions

--- R/demogromatics-functions.R
+++ R/demogromatics-functions.R
@@ -398,3 +398,17 @@ blockgroup.2000 = function(token, state = "*", county = "*", blockgroup = "*", v
 }
 
 
+#'
+
+get.counties = function(single.state, token){
+  process.api.data(fromJSON(file=url(
+    paste("http://api.census.gov/data/2000/sf3?key=", token,"&get=P001001&for=county:*&in=state:", single.state, sep = ""))))$county
+}
+
+expand.states = function(a){
+  thing = list(NULL)
+  for(i in 1:length(a)){
+    thing[[i]] = rep(names(a)[i], length(a[[i]]))
+  }
+  thing
+}
